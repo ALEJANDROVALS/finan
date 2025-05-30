d3067ca88fae9cba8129e6378612209e7ac3f165
ALEJANDROVALS <av733793@gmail.com>
2025-05-30 15:06:10 -0400

    Add files via upload

---
+++ netflix/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'finanzas-pwa-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/icon.png'
+];
+
+// Instalar Service Worker y cachear archivos
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Cache abierto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Interceptar solicitudes y servir desde caché
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Actualizar Service Worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
